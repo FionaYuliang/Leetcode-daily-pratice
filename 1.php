70ccbac12994b19cda54a5da98d0825e17a4ed6b
yangmingming <yangmingming94@gmail.com>
2020-09-09 00:10:33 +0800

    feat: 第一题完成

---
+++ 1.php
@@ -0,0 +1,14 @@
+<?php
+
+function twoSum($nums,$target){
+    for($i=0;$i<count($nums);$i++){
+        for($j=$i+1;$j<count($nums);$j++){
+            if($nums[$j] == $target-$nums[$i]){
+                return [$i,$j];
+            }
+        }
+    }
+    return [0,0];
+}
+
+echo twoSum(    [3,2,4], 6);
